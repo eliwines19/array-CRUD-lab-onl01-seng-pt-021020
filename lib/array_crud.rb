eaa5e2c96ef5721d8ed6e921aa34c5872207f6b3
Elijah wines <elijahwines84@yahoo.com>
2020-02-12 17:48:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  tv_shows = ["Game of Thrones", "Breaking Bad", "The Mandalorian", "Peaky Blinders"]
 end
 
 def add_element_to_end_of_array(array, element)
